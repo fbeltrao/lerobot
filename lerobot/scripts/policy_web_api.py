1adfb83755368d1064acdb1cf5dd97fb3bcf8c58
Francisco <frbeltra@microsoft.com>
2025-06-13 19:39:08 +0200

    Fix web policy

--- lerobot/scripts/policy_web_api.py
+++ lerobot/scripts/policy_web_api.py
@@ -21,7 +21,7 @@ from pydantic import BaseModel
 
 from lerobot.common.policies.pi0fast.modeling_pi0fast import PI0FASTPolicy
 from lerobot.common.policies.pretrained import PreTrainedPolicy
-from lerobot.common.robot_devices.control_utils import predict_action
+from lerobot.common.utils.control_utils import predict_action
 from lerobot.common.utils.utils import get_safe_torch_device
 from lerobot.configs.policies import PreTrainedConfig
 
@@ -48,7 +48,7 @@ class ObservationRequest(BaseModel):
 
     images: dict[str, str]
     state: list[float]
-    task: list[str]
+    task: str
 
 
 class PolicyResponse(BaseModel):
@@ -63,13 +63,17 @@ processor: Callable[[ObservationRequest], list[torch.Tensor]] | None = None
 
 def tensor_from_image_base64(name: str, image_base64: str, *, store_image: bool = False) -> torch.Tensor:
     """Convert a base64 encoded image to a PyTorch tensor"""
+    return torch.from_numpy(nparray_from_image_base64(name, image_base64, store_image=store_image))
+
+def nparray_from_image_base64(name: str, image_base64: str, *, store_image: bool = False) -> np.ndarray:
+    """Convert a base64 encoded image to a PyTorch tensor"""
     with io.BytesIO(base64.b64decode(image_base64)) as buffer:
         pil_img = Image.open(buffer).convert("RGB")
         if store_image:
             debug_folder = Path("outputs") / "predictions" / "input"
             debug_folder.mkdir(parents=True, exist_ok=True)
             pil_img.save(debug_folder / f"{name}.png")  # Save the image for debugging
-        return torch.from_numpy(np.array(pil_img))
+        return np.array(pil_img)
 
 
 # def image_to_tensor(image_base64: str) -> torch.Tensor:
@@ -95,21 +99,34 @@ def process_policy(request: ObservationRequest) -> list[list[float]]:
         raise HTTPException(status_code=503, detail="Policy model not loaded yet")
 
     obs = {
-        OBS_STATE_KEY: torch.tensor(request.state, dtype=torch.float32),
+        OBS_STATE_KEY: np.array(request.state),
         "task": request.task,
         "robot_type": [policy.robot_type] if hasattr(policy, "robot_type") else [""],
     }
 
     for img_name, image_value in request.images.items():
-        obs[img_name] = tensor_from_image_base64(img_name, image_value, store_image=True)
+        obs[img_name] = nparray_from_image_base64(img_name, image_value, store_image=True)
 
+    task = obs.pop("task", "")
+    robot_type = obs.pop("robot_type", "")
     predicted = predict_action(
-        obs, policy, get_safe_torch_device(policy.config.device), policy.config.use_amp
+        observation=obs, 
+        policy=policy, 
+        device=get_safe_torch_device(policy.config.device), 
+        use_amp=policy.config.use_amp,
+        task=task,
+        robot_type=robot_type,
     )
 
     # Convert the predicted actions to a list of lists
     actions = []
     actions.append(predicted.cpu().numpy().tolist())
+
+    if "_action_queue" in policy.__dict__:
+        # If the policy has an action queue, we need to pop the first action
+        while len(policy._action_queue) > 0:
+            action = policy._action_queue.popleft()
+            actions.append(action.cpu().numpy()[0].tolist())
 
     # actions: list[list[float]] = []
     # with torch.no_grad():
@@ -129,15 +146,20 @@ def process_policy(request: ObservationRequest) -> list[list[float]]:
 async def load_model():
     """Load the model when the application starts"""
     global policy, processor
-    print("Loading policy model... This may take a while.")
 
     pretrained_name_or_path = "fbeltrao/so101_unplug_cable_4"
     revision = "steps_10_000"
-    policy_config = PreTrainedConfig.from_pretrained(
-        pretrained_name_or_path=pretrained_name_or_path, revision=revision
-    )
+
+    pretrained_name_or_path = "fbeltrao/pi0fast_so101_multi_task"
+    revision = "v5000steps"
+
+    print(f"Loading policy model {pretrained_name_or_path} (revision={revision})... This may take a while.")
+
+    # policy_config = PreTrainedConfig.from_pretrained(
+    #     pretrained_name_or_path=pretrained_name_or_path, revision=revision
+    # )
     # policy_config.device = "cpu"  # Force CPU for compatibility
-    policy = PI0FASTPolicy.from_pretrained(pretrained_name_or_path, revision=revision, config=policy_config)
+    policy = PI0FASTPolicy.from_pretrained(pretrained_name_or_path, revision=revision)
     policy.eval()
     policy.reset()
 
