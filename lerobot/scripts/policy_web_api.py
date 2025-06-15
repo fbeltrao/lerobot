92ecb0d40dd83676890730ca7e0971b0b3a31d85
Francisco <frbeltra@microsoft.com>
2025-06-15 18:44:22 +0200

    Add ACT policy to spike

--- lerobot/scripts/policy_web_api.py
+++ lerobot/scripts/policy_web_api.py
@@ -7,6 +7,7 @@
 # - pillow
 # pip install fastapi uvicorn transformers==4.48.1 pydantic pillow
 import base64
+from dataclasses import dataclass
 import io
 from pathlib import Path
 from typing import Callable
@@ -19,10 +20,13 @@ from fastapi.middleware.cors import CORSMiddleware
 from PIL import Image
 from pydantic import BaseModel
 
+from lerobot.common.policies.act.modeling_act import ACTPolicy
+from lerobot.common.policies.factory import make_policy
 from lerobot.common.policies.pi0fast.modeling_pi0fast import PI0FASTPolicy
 from lerobot.common.policies.pretrained import PreTrainedPolicy
 from lerobot.common.utils.control_utils import predict_action
 from lerobot.common.utils.utils import get_safe_torch_device
+from lerobot.configs import parser
 from lerobot.configs.policies import PreTrainedConfig
 
 # Initialize FastAPI app
@@ -77,19 +81,6 @@ def nparray_from_image_base64(name: str, image_base64: str, *, store_image: bool
         return np.array(pil_img)
 
 
-# def image_to_tensor(image_base64: str) -> torch.Tensor:
-#     image_array: np.ndarray | None = None
-#     with io.BytesIO(base64.b64decode(image_base64)) as buffer:
-#         pil_img = Image.open(buffer).convert("RGB")
-#         image_array = np.array(pil_img)
-
-#     img = torch.from_numpy(image_array)
-
-#     v = img.type(torch.float32) / 255
-#     result = v.permute(2, 0, 1).unsqueeze(0)
-#     return result
-
-
 OBS_STATE_KEY = "observation.state"
 
 
@@ -100,7 +91,7 @@ def process_policy(request: ObservationRequest) -> list[list[float]]:
         raise HTTPException(status_code=503, detail="Policy model not loaded yet")
 
     obs = {
-        OBS_STATE_KEY: np.array(request.state),
+        OBS_STATE_KEY: np.array(request.state, dtype=np.float32),
     }
 
     for img_name, image_value in request.images.items():
@@ -125,42 +116,34 @@ def process_policy(request: ObservationRequest) -> list[list[float]]:
             action = policy._action_queue.popleft()
             actions.append(action.cpu().numpy()[0].tolist())
 
-    # actions: list[list[float]] = []
-    # with torch.no_grad():
-    #     print(f"Running policy with state {obs}...")
-    #     action = policy.select_action(obs)
-    #     actions.append(action.cpu().numpy()[0].tolist())
-    #     if "_action_queue" in policy.__dict__:
-    #         # If the policy has an action queue, we need to pop the first action
-    #         while len(policy._action_queue) > 0:
-    #             action = policy._action_queue.popleft()
-    #             actions.append(action.cpu().numpy()[0].tolist())
-
     return actions
 
 
 @app.on_event("startup")
-async def load_model():
-    """Load the model when the application starts"""
+async def on_startup():
+    """Event handler for application startup"""
     global policy, processor
+    policy, processor = await load_model()
 
-    pretrained_name_or_path = "fbeltrao/so101_unplug_cable_4"
-    revision = "steps_10_000"
+async def load_model() -> tuple[PreTrainedPolicy, Callable[[ObservationRequest], list[torch.Tensor]]] :
+    """Load the model when the application starts"""
+    
+    # pretrained_name_or_path = "fbeltrao/pi0fast_so101_multi_task"
+    # revision = "v5000steps"
 
-    pretrained_name_or_path = "fbeltrao/pi0fast_so101_multi_task"
-    revision = "v5000steps"
+    # print(f"Loading policy model {pretrained_name_or_path} (revision={revision})... This may take a while.")
 
-    print(f"Loading policy model {pretrained_name_or_path} (revision={revision})... This may take a while.")
+    # policy = PI0FASTPolicy.from_pretrained(pretrained_name_or_path, revision=revision)
+    
 
-    # policy_config = PreTrainedConfig.from_pretrained(
-    #     pretrained_name_or_path=pretrained_name_or_path, revision=revision
-    # )
-    # policy_config.device = "cpu"  # Force CPU for compatibility
-    policy = PI0FASTPolicy.from_pretrained(pretrained_name_or_path, revision=revision)
+    pretrained_name_or_path = "fbeltrao/act_so101_multi_task"
+    revision= "v5000steps"
+    policy = ACTPolicy.from_pretrained(pretrained_name_or_path, revision=revision)
+
     policy.eval()
     policy.reset()
 
-    processor = process_policy  # Assign the processing function
+    return policy, process_policy
 
 
 @app.post("/predict", response_model=PolicyResponse)
@@ -181,7 +164,6 @@ async def predict(request: ObservationRequest) -> PolicyResponse:
 async def health_check():
     """Endpoint to check if the API is running"""
     return {"status": "healthy", "model_loaded": policy is not None}
-
 
 if __name__ == "__main__":
     # Run the server on 0.0.0.0 to accept connections from any IP address
